4e588563aeba4e0143c10c5f70488d0db49f1104
fviana239 <fviana386@gmail.com>
2021-03-18 21:47:20 -0300

    Add de exemplo de tabela SQL

---
+++ exemplo_estado_esp.sql
@@ -0,0 +1,16 @@
+
+-- Table structure for table `teste`
+--
+
+CREATE TABLE `teste` (
+  `id` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `outros` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+
+
+INSERT INTO `teste` (`id`, `status`, `outros`) VALUES
+(1, 0, 0);
+
